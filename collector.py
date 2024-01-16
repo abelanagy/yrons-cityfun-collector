95ec3073c74aee57f6e0343f1654d6ef24807e7a
Mikula Lajos (Thinkpad X230) <mikulalajos.jr@gmail.com>
2024-01-16 12:18:56 +0100

    Added dir naming (todo #3), added requirements for script for easier bootstrapping, added .gitignore, added verbose mode and handled edge cases

--- collector.py
+++ collector.py
@@ -1,6 +1,5 @@
 # TODO add file save dir dialog
 # TODO add progress bar
-# TODO add dir naming
 
 import argparse
 import json
@@ -13,6 +12,8 @@ parser = argparse.ArgumentParser(description='Collects the images from yrons cit
 
 parser.add_argument('filename', help='the path of the file exported from phpmyadmin')
 parser.add_argument('-z', '--zip', help='zips the images after collection', action='store_true')
+parser.add_argument('-v', '--verbose', help='prints more information', action='store_true')
+parser.add_argument('-d', '--dir', dest='dir', help='the directory to save the images to', default='images')
 
 args = parser.parse_args()
 
@@ -20,24 +21,32 @@ try:
     with open(args.filename) as f:
         data = json.load(f)
         if data[2]['name'] != 'fileUploads' or data[2]['database'] != 'cityfun':
-            print('Wrong file format')
+            print('Wrong file format', data[2]['name'], data[2]['database'])
             exit() 
         else:
             rows = data[2]['data']
-            os.mkdir('images')
+            if args.verbose:
+                print(f"Found {len(rows)} images in table, starting download")
+            try:
+               os.mkdir(args.dir)
+            except:
+                print('Directory already exists')
+                pass
             for row in rows:
                 fileExtension = row['filename'].split('.')[-1]
                 filename = ''.join(row['filename'].split('.')[:-1])
                 res = requests.get(row['fullLink'], stream=True)
                 if res.status_code == 200:
-                    with open(f"images/{row['jwt']}-{filename}.{fileExtension}", 'wb') as f:
+                    with open(f"{args.dir}/{row['jwt']}-{filename}.{fileExtension}", 'wb') as f:
                         shutil.copyfileobj(res.raw, f)
+                        if args.verbose:
+                            print(f"Downloading {filename}.{fileExtension}, and saving as {row['jwt']}-{filename}.{fileExtension}")
                 else:
                     print('Error while downloading image')
                     exit()
     if args.zip:
-        shutil.make_archive('images', 'zip', 'images')
-        shutil.rmtree('images')
+        shutil.make_archive('images', 'zip', args.dir)
+        shutil.rmtree(args.dir)
     print('Done')
 except FileNotFoundError:
     print('File not found')
